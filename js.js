ecc17f303fb4bdc9915cb303f8154639a8cc0982
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-10-19 12:27:51 +0300

    optim app

--- js.js
+++ js.js
@@ -8,9 +8,8 @@ let vlSelect;
 // запоминаем значение во нешнюю переменную что бы функция могла считать значение
 function selectFn() {
   vlSelect = select.value;
+  console.log(vlSelect);
 }
-
-let inp = document.querySelector('input');
 
 //функцию которая создает элементы в качестве аргумента принимает строковый параметр
 function criateElem(mes) {
@@ -28,32 +27,56 @@ function criateElem(mes) {
 
 function FnInsert() {
   let prop = vlSelect; //получаем данные с селекта
-  let selector = inp.value; //получаем данные с инпута
-
-  try {
-    if (document.querySelector(selector) && prop) {
-      document.querySelector(selector).style = prop;
-    } else {
-      //вставляем элемент в боди полсе начала вторым аргументом передаем нашу
-      // самописную функцию criateElem(mes)
-      document.body.insertAdjacentElement(
-        'afterbegin',
-        criateElem('вы ввели некорректный селектор либо не выбрали свойство'),
-      );
-      document.querySelector('.overlay').addEventListener('click', function(e) {
-        document.querySelector('.overlay').remove();
-      });
-      console.log(' try');
-    }
-  } catch (e) {
-    document.body.insertAdjacentElement(
-      'afterbegin',
-      criateElem('поле пустое введите селектор'),
-    );
+  let selector = document.querySelector('input').value; //получаем данные с инпута
+  if (selector == '' ) {
+    console.log(selector);
+    console.log(1111);
+    document.body.append(criateElem('вы не ввели селектор '));
     document.querySelector('.overlay').addEventListener('click', function(e) {
       document.querySelector('.overlay').remove();
-      console.log(1);
     });
-    console.log('catch');
+
+  } else if(document.querySelector(selector)==null){
+    document.body.append(criateElem('вы ввели некорректный селектор '));
+    document.querySelector('.overlay').addEventListener('click', function(e) {
+      document.querySelector('.overlay').remove();
+    });
   }
+  else if (prop == undefined) {
+    document.body.append(criateElem('вы не выбрали свойство '));
+    document.querySelector('.overlay').addEventListener('click', function(e) {
+      document.querySelector('.overlay').remove();
+    });
+  }
+  else if (prop == 'undefined') {
+    document.body.append(criateElem('вы не выбрали свойство '));
+    document.querySelector('.overlay').addEventListener('click', function(e) {
+      document.querySelector('.overlay').remove();
+    });
+  }
+   else {
+    document.querySelector(selector).style = prop;
+  }
+  // try {
+  //   if (document.querySelector(selector) && prop) {
+  //     document.querySelector(selector).style = prop;
+  //   } else {
+  //     вставляем элемент в боди полсе начала вторым аргументом передаем нашу
+  //     самописную функцию criateElem(mes)
+  //     document.body.append(
+  //       criateElem('вы ввели некорректный селектор либо не выбрали свойство'),
+  //     );
+  //     document.querySelector('.overlay').addEventListener('click', function(e) {
+  //       document.querySelector('.overlay').remove();
+  //     });
+  //     console.log(' try');
+  //   }
+  // } catch (e) {
+  //   document.body.append(criateElem('поле пустое введите селектор'));
+  //   document.querySelector('.overlay').addEventListener('click', function(e) {
+  //     document.querySelector('.overlay').remove();
+  //     console.log(1);
+  //   });
+  //   console.log('catch');
+  // }
 }
